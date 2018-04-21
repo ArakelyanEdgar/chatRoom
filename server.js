e955fb996c6302615e0d349eaffdb4483404ff6f
Edgar Arakelyan <arakee1@unlv.nevada.edu>
2018-04-21 13:20:42 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+let port = process.env.PORT || 3000
+
+
+
+app.listen(port, () => {
+    console.log(`Server deployed on port ${port}`)
+})
